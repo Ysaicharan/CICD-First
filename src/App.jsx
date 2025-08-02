7e574ea7773868774b774bb4acfa24558e68f947
Ysaicharan <2300090051@kluniversity.in>
2025-08-02 16:41:28 +0530

    version 6

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-return (
-   <h1>HELLO WORLD</h1>
-)
+function App(){
+  const [count,setCount]=useState()
 
+  return (
+    <>
+   <h1>HELLO WORLD</h1>
+   </>
+)
+}
 export default App;
 
